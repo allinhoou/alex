60a577a8e1a5cc9a99e29df963f8077c70c31d40
allinhoou <128159534+allinhoou@users.noreply.github.com>
2024-04-26 09:10:55 -0300

    Add files via upload

---
+++ JsSintaxe-main/conversoes.js
@@ -0,0 +1,61 @@
+//coversao implicita
+
+
+/*const numero = 456;
+const numeroString = "456";
+
+console.log(numero === numeroString);*/
+
+/////////////////////////////////////////
+
+/*const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString);*/
+
+/////////////////////////////////////////
+
+//number()
+//string()
+/*
+const numero = 456;
+const numeroString = Number("456");
+
+console.log(numero / numeroString);*/
+
+////////////////////////////////////////////
+/*
+let telefone = 12341234;
+console.log("O telefone é " + String(telefone));*/
+
+////////////////////////////////////////////
+/*
+let telefone = 12341234;
+console.log("O telefone é " + telefone.toString());*/
+
+///////////////////////////////////////////
+
+/*let usuarioConectado = false;
+console.log(String(usuarioConectado)); 
+usuarioConectado = true;
+console.log(String(usuarioConectado)); */
+
+///////////////////////////////////////////////
+
+/*
+let largura = "10";
+let altura = "5";
+console.log(Number(largura) * Number(altura));*/
+
+//////////////////////////////////////////////////
+/*
+let largura = "10";
+let altura = "5";
+console.log( + largura * + altura);*/
+
+////////////////////////////////////////////
+
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString);
---
+++ JsSintaxe-main/ex.js
@@ -0,0 +1,59 @@
+
+// var nwme = "Luiz";
+// var idade = 17;
+
+// console.log(`Oi, me chamo ${nwme} e tenho ${idade} anos.`);
+
+/////////////////////////////////////////////////////
+
+var number = 9;
+var number1 = 2;
+
+var soma = number + number1;
+var subtração = number - number1;
+var multiplicação = number * number1;
+var divisão = number / number1;
+
+console.log(`As operações basicas para ${number} e ${number1} são: Soma = ${soma}, Subtração = ${subtração}, Multiplicação = ${multiplicação}, Divisão = ${divisão}.`);
+
+///////////////////////////////////////////////////////
+
+// var idade = prompt("Digite sua idade");
+// var number = parseFloat(idade);
+
+// if(number >= 18){
+//     alert("Seja bem-vindo");
+// }else if(number < 18){
+//     alert("CAI FORA DAQUI!!!");
+// }
+
+// console.log(number / 2);
+
+////////////////////////////////////////////////////////
+
+// for( i = 0; i <= 10; i ++){
+//     console.log(i)
+// }
+
+////////////////////////////////////////////////////////
+
+// var estilosMusicais = ["Rock", "Jazz", "Blues", "Hip Hop", "Funk"];
+
+// for (var i = 0; i < estilosMusicais.length; i++) {
+//     console.log((i + 1) + ". " + estilosMusicais[i]);
+// }
+
+////////////////////////////////////////
+
+// var LivroAtb = {
+//     Titulo: 1984,
+//     Autor: "John Orwell",
+//     N_paginas: 308,
+//     Editora: "Ulisseia",
+//     ISBN: 8535914846
+// };
+
+// console.log(`Livro ${LivroAtb.Titulo}, Autor ${LivroAtb.Autor}, Nº paginas ${LivroAtb.N_paginas}, Editora ${LivroAtb.Editora}, Codigo da versão ${LivroAtb.ISBN}`)
+
+// console.log(LivroAtb);
+
